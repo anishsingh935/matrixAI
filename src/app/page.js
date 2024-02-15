7411dd52a18a8a56d2adb9cb6c579ca05cbe5edb
Sahil Shile <sahilshile25@gmail.com>
2024-02-16 01:58:59 +0530

    Changes by Sahil

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
-
+import FormLeft from "@/components/FormLeft";
+import Cards from "@/components/Cards";
 export default function Home() {
   return (
-    <></>
+    <div className="flex items-center justify-start">
+      {/* <FormLeft></FormLeft> */}
+      {/* <FormRight></FormRight> */}
+      <Cards></Cards>
+    </div>
   );
 }
--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,13 +1,28 @@
-import Logo from './logo.svg';
-import Setting from './setting.svg';
-import Info from './info.svg';
-import Avtar from './avtar.svg';
-import ArrowDown from './arrow-down.svg';
-
+import Logo from "./logo.svg";
+import Setting from "./setting.svg";
+import Info from "./info.svg";
+import Avtar from "./avtar.svg";
+import ArrowDown from "./arrow-down.svg";
+import User from "./user.svg";
+import More from "./more.svg";
+import Zip from "./zip.svg";
+import Facebook from "./fb.svg";
+import Insta from "./int.svg";
+import Twitter from "./twt.svg";
+import Intranet from "./intranet.svg";
+import Read from "./read.svg";
 export {
-    Logo,
-    Setting,
-    Info,
-    Avtar,
-    ArrowDown,
-}
+  Logo,
+  Setting,
+  Info,
+  Avtar,
+  ArrowDown,
+  User,
+  More,
+  Zip,
+  Facebook,
+  Insta,
+  Twitter,
+  Intranet,
+  Read,
+};
