b6f07ff747a1bddc9aaa65a447f35460c13efda8
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-02-15 22:30:55 +0530

    nav complete

---
+++ src/assets/svg/index.js
@@ -0,0 +1,13 @@
+import Logo from './logo.svg';
+import Setting from './setting.svg';
+import Info from './info.svg';
+import Avtar from './avtar.svg';
+import ArrowDown from './arrow-down.svg';
+
+export {
+    Logo,
+    Setting,
+    Info,
+    Avtar,
+    ArrowDown,
+}
