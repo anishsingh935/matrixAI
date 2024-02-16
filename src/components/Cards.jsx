745ebf86930f888760f7dc2fca5b8c905c3eeb5f
Sahil Shile <sahilshile25@gmail.com>
2024-02-17 00:00:03 +0530

    feat/buttons

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { More } from "@/assets/svg";
 import Card from "./common/Card";
 // import Image from "next/image";
+import { Message, Timer } from "@/assets/svg";
 import { Facebook, Insta, Twitter, Intranet, Read } from "@/assets/svg";
 const Cards = () => {
   const data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
@@ -78,6 +79,33 @@ const Cards = () => {
                 <Image src={Facebook}></Image>
                 <Image src={Intranet}></Image>
                 <Image src={Read}></Image>
+              </span>
+              <span
+                style={{
+                  display: "flex",
+                  gap: "8px",
+                }}
+              >
+                <span
+                  style={{
+                    background: "#C7EBB0",
+                    display: "flex",
+                    alignItems: "center",
+                    borderRadius: "2rem",
+                    paddingInline: "9px",
+                  }}
+                >
+                  <Image src={Message} width={40} height={40}></Image>
+                  <p
+                    style={{
+                      fontWeight: "700",
+                      fontSize: "14px",
+                    }}
+                  >
+                    Open Chat
+                  </p>
+                </span>
+                <Image src={Timer}></Image>
               </span>
             </span>
           </div>
